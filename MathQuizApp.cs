b72978d6671b8202bb4d7e7fa1e8a54b4314a354
Jakub768 <kubabilinski03@gmail.com>
2026-07-28 16:07:36 +0100

    fix unnecessary instantiation

--- MathQuizApp.cs
+++ MathQuizApp.cs
@@ -11,18 +11,16 @@ public class MathQuizApp
 
     public void Run()
     {
-        UserGameInteract interact = new UserGameInteract();
         Operation chosenOp;
 
-        Menu m = interact.PromptMenu();
-
+        Menu m = _interact.PromptMenu();
         switch (m)
         {
             case Menu.History:
                 Console.WriteLine("no");
                 break;
             case Menu.Play: 
-                chosenOp = interact.PromptOperation();
+                chosenOp = _interact.PromptOperation();
                 _choiceHandler.initGame(chosenOp);
                 break;
             case Menu.Exit:
